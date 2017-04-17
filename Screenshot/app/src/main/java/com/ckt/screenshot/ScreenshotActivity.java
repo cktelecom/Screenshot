0e1a5cb9cb982cbe3fed07682f0502799b318af3
chengzhi.hou <chengzhi.hou@ck-telecom.com>
2017-04-17 20:53:50 +0800

    implement save and share function
    Change-Id: I7a396c4a905751b3db4f1c694d3cb91e894094ca

--- Screenshot/app/src/main/java/com/ckt/screenshot/ScreenshotActivity.java
+++ Screenshot/app/src/main/java/com/ckt/screenshot/ScreenshotActivity.java
@@ -1,21 +1,30 @@
 package com.ckt.screenshot;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
+import java.io.File;
+
 public class ScreenshotActivity extends AppCompatActivity {
     private static final String TAG = "ScreenshotActivity";
-    private ImageView imgScreenshot;
+    private ImageView mImgScreenshot;
+    private Bitmap mBitmap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        takeScreenshot();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.crop_view);
-        imgScreenshot = (ImageView) findViewById(R.id.img_screenshot);
+        mImgScreenshot = (ImageView) findViewById(R.id.img_screenshot);
 
         loadScreenshot();
     }
@@ -36,7 +45,9 @@ public class ScreenshotActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_share) {
+            saveScreenshot();
             share();
+            finish();
             return true;
         }
         if (id == R.id.action_cancel) {
@@ -45,22 +56,49 @@ public class ScreenshotActivity extends AppCompatActivity {
             return true;
         }
         if (id == R.id.action_done) {
-            takeScreenshot();
+            saveScreenshot();
+            finish();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
     public void loadScreenshot() {
-        imgScreenshot.setImageResource(R.drawable.sc_example);
+//        mImgScreenshot.setImageResource(R.drawable.sc_example);
+        mImgScreenshot.setImageBitmap(mBitmap);
 
-    }
-
-    public void share() {
-        Log.d(TAG, "share: ");
     }
 
     public void takeScreenshot() {
         Log.d(TAG, "takeScreenshot: ");
+
+        Utils.setFilePathAndName();
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        mBitmap = Screenshot.takeScreen(width, height, getWindow().getDecorView());
+    }
+
+    /**
+     * save screenshot to sdcard/Pictures/Screenshots/
+     */
+    public void saveScreenshot() {
+        Log.d(TAG, "saveScreenshot to storage");
+        Utils.saveScreenshotFile(mBitmap);
+    }
+
+    /**
+     * share screenshot to other apps
+     */
+    public void share() {
+        Log.d(TAG, "share pics");
+        final File imageFile = new File(Utils.getImageFilePath());
+        // Create a share intent
+        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+        sharingIntent.setType("image/png");
+        sharingIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(imageFile));
+        startActivity(Intent.createChooser(sharingIntent, getResources().getText(R.string.share)));
     }
 }
---
+++ Screenshot/app/src/main/java/com/ckt/screenshot/Utils.java
@@ -0,0 +1,57 @@
+package com.ckt.screenshot;
+
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class Utils {
+    private static final String TAG = "ScreenshotUtils";
+    private static final String SCREENSHOT_FILE_NAME_TEMPLATE = "Screenshot_%s.png";
+    private static final String mScreenshotDir = "/storage/emulated/0/Pictures/Screenshots/";
+    private static String mImageFilePath;
+
+    /**
+     * file name would be like "Screenshot_20170417_222222.png"
+     */
+    public static void setFilePathAndName() {
+        long imageTime = System.currentTimeMillis();
+        String imageDate = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date(imageTime));
+        String imageFileName = String.format(SCREENSHOT_FILE_NAME_TEMPLATE, imageDate);
+        mImageFilePath = new File(mScreenshotDir, imageFileName).getAbsolutePath();
+        mImageFilePath = mScreenshotDir + imageFileName;
+        Log.d(TAG, "setFilePathAndName: file name: " + mImageFilePath);
+    }
+
+    /**
+     * @return screenshot image file's absolute path and name
+     */
+    public static String getImageFilePath() {
+        return mImageFilePath;
+    }
+
+
+    /**
+     * save screenshot to sdcard
+     * @param bitmap the image source to be save
+     */
+    public static void saveScreenshotFile(Bitmap bitmap) {
+        try {
+            // Save
+            OutputStream out = new FileOutputStream(getImageFilePath());
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+            out.flush();
+            out.close();
+            Log.d(TAG, "saveScreenshotFile: success");
+        } catch (Throwable e) {
+            // Several error may come out with file handling or OOM'
+            Log.e(TAG, "saveScreenshotFile: can't sav screenshot, missing storage permissions");
+            e.printStackTrace();
+        }
+    }
+}
