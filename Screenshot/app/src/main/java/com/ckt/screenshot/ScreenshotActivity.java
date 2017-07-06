052b357759986b5acb2c150e7511bf8431aed81a
sida.liu <sida.liu@ck-telecom.com>
2017-07-06 11:16:00 +0800

    Add permission request in ScreenshotActivity.
    Change-Id: I506763f9b78580bce6b857d1738fdd661e2eafda

--- Screenshot/app/src/main/java/com/ckt/screenshot/ScreenshotActivity.java
+++ Screenshot/app/src/main/java/com/ckt/screenshot/ScreenshotActivity.java
@@ -1,17 +1,29 @@
 package com.ckt.screenshot;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class ScreenshotActivity extends AppCompatActivity {
     private static final String TAG = "ScreenshotActivity";
 
+    public static final int REQUEST_DRAW_OVER = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestPermission();
     }
 
     @Override
@@ -28,5 +40,35 @@ public class ScreenshotActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void requestPermission() {
+        Log.d(TAG, "requestPermission");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        } else {
+            requestDrawOverPermission();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            requestDrawOverPermission();
+        }
+    }
+
+    public void requestDrawOverPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, REQUEST_DRAW_OVER);
+            }
+        }
     }
 }
